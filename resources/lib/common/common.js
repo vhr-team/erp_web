d9941736756bd5a7e4c4cc4a5862c4181d55e4c7
xhh <303158131@qq.com>
2020-04-16 10:09:43 +0800

    路径问题

--- resources/lib/common/common.js
+++ resources/lib/common/common.js
@@ -1,5 +1,6 @@
 let api = 'http://127.0.0.1:8001/';
 let baseUrl = "http://39.106.71.75:8888/";
+let loginUrl='http://localhost:8888/ERP_WEB/login.html';
 
 // 下次再发送ajax请求把token带到后台
 let token = $.cookie("TOKEN");
@@ -13,11 +14,11 @@ $.ajaxSetup({
 
 // 如果访问登陆页面之外的页面 并且还没有登陆成功之后写入cookie的token就转到登陆页面
 if (token == undefined) {
-    if (window.location != "http://localhost:8888/ERP_WEB/login.html") {
-        window.top.location = '/ERP_WEB/login.html';
+    if (window.location != loginUrl) {
+        window.top.location = loginUrl;
     }
 } else {
-    if (window.location != "http://localhost:8888/ERP_WEB/login.html") {
+    if (window.location != loginUrl) {
         $.ajax({
             url: api + "login/checkLogin",
             async: true,// 同步处理
@@ -25,11 +26,11 @@ if (token == undefined) {
             dataType: "json",
             success: function (res) {
                 if (res.code == -1) {
-                    window.top.location = '/ERP_WEB/login.html';
+                    window.top.location = loginUrl;
                 }
             },
             error: function (res) {
-                window.top.location = '/ERP_WEB/login.html';
+                window.top.location = loginUrl;
             }
         });
     }
