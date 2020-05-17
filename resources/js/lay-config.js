a8e46c290ecb32e894d3e7754f24ab230c81baf6
xhh <303158131@qq.com>
2020-05-17 22:29:58 +0800

    1.2完毕

--- resources/js/lay-config.js
+++ resources/js/lay-config.js
@@ -8,11 +8,13 @@ window.rootPath = (function (src) {
     src = document.scripts[document.scripts.length - 1].src;
     return src.substring(0, src.lastIndexOf("/") + 1);
 })();
-
 layui.config({
     base: rootPath + "lay-module/",
     version: true
 }).extend({
+    // febs: 'febs/lay/modules/febs',
+    // validate: 'febs/lay/modules/validate',
+
     layuimini: "layuimini/layuimini", // layuimini扩展
     step: 'step-lay/step', // 分步表单扩展
     treeTable: 'treetable-lay/treeTable', //table树形扩展
@@ -28,7 +30,6 @@ layui.config({
     pearMenu:"PearAdmin/extends/pearMenu",
     pearOper:"PearAdmin/extends/pearOper",
     pearHash:"PearAdmin/extends/pearHash",
-    pearHash:"PearAdmin/extends/code",
     pearAdmin:"PearAdmin/extends/pearAdmin",
     pearFrame:"PearAdmin/extends/pearFrame",
     pearSocial:"PearAdmin/extends/pearSocial",
@@ -37,4 +38,7 @@ layui.config({
 
     // Febs Admin
     apexcharts: 'febs/lay/extends/apexcharts.min',
+}).define([], function (exports) {
+    console.log("\n %c Erp_Web %c https://github.com/XiaoHuiHuiBlog %c 如果该项目对您有帮助的话，还请点个star给予精神支持！🐤", "color: #fff; font-size: .84rem;background: #366ed8; padding:5px 0;", "font-size: .84rem;background: #fff; border: 2px solid #b0e0a8;border-left: none; padding:3px 0;"," font-size: .84rem;background: #fcf9ec; padding:5px 0;margin-left: 8px");
+    exports('index', {});
 });
