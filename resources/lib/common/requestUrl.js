8802f422c60966f51726b6efcbd9e60c68649751
Zeng-Dong-Song <3112460595@qq.com>
2020-05-23 20:29:35 +0800

    页面美化

--- resources/lib/common/requestUrl.js
+++ resources/lib/common/requestUrl.js
@@ -1,4 +1,4 @@
 let api1 = 'http://127.0.0.1:8002/';
 let api3 = 'http://127.0.0.1:8004/';
-let productionApi = 'http://localhost:8003/';
+let productionApi = 'http://127.0.0.1:8003/';
 let productionDingJian = 'http://127.0.0.1:8008/';
